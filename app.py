53914ee6b9971b60028a33661fbe71d869466448
Fabian Anguiano <fabiananguiano@gmail.com>
2020-02-21 22:34:59 -0600

    refactoring

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+# Brings packages in
 from flask import Flask, render_template, url_for
 
+
+# Nameing of the app
 app = Flask(__name__)
 
+
+# Routing for app 
 @app.route('/')
 def index():
     return render_template('index.html')
