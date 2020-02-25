750ef6c6d4637526f4dbe5ed8f66c65053310150
Fabian Anguiano <fabiananguiano@gmail.com>
2020-02-24 23:08:47 -0600

    started databse implemenaio

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 # Brings packages in
 from flask import Flask, render_template, url_for
+from flask import flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 # Nameing of the app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Interger, primary_key = True)
+    content = db.Column(db.string(200), nullable = False)
+    completed = db.Column(db.Integer, defualt = 0)
+    date_created = db.Column(db.Datetime, defualt=datetime.utcnow)
+
 
 
 # Routing for app 
